0e1ab529a2ec248a83fcd55be62323ec5a43dbd2
Davide Principi <davide.principi@nethesis.it>
2018-06-28 10:50:42 +0200

    Fix html theme path

--- admin-manual/conf.py
+++ admin-manual/conf.py
@@ -10,13 +10,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-
-try:
-    import sphinx_rtd_theme
-    import sphinx_bootstrap_theme
-except ImportError:
-    pass
+import os
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -97,7 +92,6 @@ pygments_style = 'sphinx'
 
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
 html_theme_options = {
         'navbar_title': 'NethVoice',
         'navbar_pagenav': True,
